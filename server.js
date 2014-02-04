ab73d07ebb742a44ece4aecbb9537cc3cda15f61
Steven Schmatz <stevenschmatz@gmail.com>
2014-02-04 10:13:55 -0500

    created the node server in a function to be exported as a module

--- server.js
+++ server.js
@@ -3,11 +3,17 @@
  */
 var http = require('http');
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-Type':'text/plain'});
-    response.write("Hello World!");
-    response.end();
-}).listen(8888);
+function start() {
+    function onRequest(request, response) {
+        console.log('request received');
+        response.writeHead(200, {'Content-type':'text/plain'});
+        response.write("Hello World");
+        response.end();
+    }
 
-console.log('Server has started.');
+    http.createServer(onRequest).listen(8888);
+    console.log('server has started');
 
+}
+
+exports.start = start;
