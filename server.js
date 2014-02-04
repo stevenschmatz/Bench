df67660a705437c3d058a8c2c3b5e463f6f42409
Steven Schmatz <stevenschmatz@gmail.com>
2014-02-04 09:39:59 -0500

    added server.js, did some other small changes to UI

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by stevenschmatz on 2/4/14.
+ */
+var http = require('http');
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}).listen(8888);
