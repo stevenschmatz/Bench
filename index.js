d42ab1697cd90a1c9373607ee4f0e2497c30d960
Steven Schmatz <stevenschmatz@gmail.com>
2014-02-04 10:35:27 -0500

    created router.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
  * Created by stevenschmatz on 2/4/14.
  */
 var server = require("./server");
+var router = require("./router");
 
-server.start();
+server.start(router.route);
--- server.js
+++ server.js
@@ -2,10 +2,15 @@
  * Created by stevenschmatz on 2/4/14.
  */
 var http = require('http');
+var url = require('url');
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
-        console.log('request received');
+        var pathname = url.parse(request.url).pathname;
+        console.log('Request for ' + pathname + ' received.');
+
+        route(pathname);
+
         response.writeHead(200, {'Content-type':'text/plain'});
         response.write("Hello World");
         response.end();
